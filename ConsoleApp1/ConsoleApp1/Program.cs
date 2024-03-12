b222c4ae6856146456b899d036d15f6e29bf252d
s16608 <s16608@pjwstk.edu.pl>
2024-03-12 22:51:58 +0100

    Modyfikacja 3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,8 @@ class Program
         Console.WriteLine("Modyfikacja 1");
         
         Console.WriteLine("Modyfikacja 2");
+        
+        Console.WriteLine("Modyfikacja 3");
             
     }
 }
